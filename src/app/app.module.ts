296c8f52586edb383c6943ed17925664d2c4d411
WhatTheTea <fox228.af@gmail.com>
2023-03-12 12:16:57 +0200

    desired functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NotesComponent } from './notes/notes.component';
@@ -10,7 +11,9 @@ import { NotesComponent } from './notes/notes.component';
     NotesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.css']
 })
 export class NotesComponent {
+  constructor(private formBuilder : FormBuilder) {}
 
+  public notes : {name: string|null, description: string|null}[] = []
+
+  public notesForm = this.formBuilder.group({
+    name: '',
+    description: ''
+  })
+
+  public onSubmit(): void 
+  {
+    this.notes.push(this.notesForm.value as {name: string|null, description: string|null})
+    this.notesForm.reset()
+  }
 }
