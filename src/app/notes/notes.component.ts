c67938e5cc737ad425c38771790923a253f84820
WhatTheTea <fox228.af@gmail.com>
2023-03-12 18:39:28 +0200

    deletion fix

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -10,7 +10,7 @@ export class NotesComponent {
   constructor(private formBuilder : FormBuilder) {}
 
   public notes : Note[] = []
-  public selectedNote : Note | null = null
+  public selectedNotes : string[] = []
   public notesForm = this.formBuilder.group(new Note(null,null))
 
   public onSubmit(): void 
@@ -20,10 +20,17 @@ export class NotesComponent {
   }
   public onDelete(): void 
   {
-    if (this.selectedNote !== null)
+    if (this.selectedNotes.length !== 0)
     {
-      const noteIndex = this.notes.indexOf(this.selectedNote as Note)
-      this.notes.splice(noteIndex-1, 1)
+      for (var strnote of this.selectedNotes)
+      {
+        const note = new Note(strnote.split(':')[0],strnote.split(':')[1].slice(1))
+        let i = -1
+        this.notes.forEach((value, index) => i = value.name === note.name && value.description === note.description ? index : i)
+        if (i > -1) this.notes.splice(i,1)
+        console.log(note)
+      }
+      
     }
   }
 }
